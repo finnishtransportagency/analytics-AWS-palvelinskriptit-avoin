dcc819d25e1115dd4bdf2584d06edff5c53d9673
Ossi <compvter2@gmail.com>
2019-07-22 18:28:41 +0300

    added taskdefinition file

--- AIS/WebSocketReader/deploy.sh
+++ AIS/WebSocketReader/deploy.sh
@@ -7,13 +7,15 @@ ECS_TASK_DEFINITION_TEMPLATE="task.json"
 ECS_TASK="task-ecs.json"
 ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${CODEBUILD_RESOLVED_SOURCE_VERSION}"
 
-# Create Task Definition replica
+# Create task definition replica
 
 sed -e "s;%ECR_IMAGE%;${ECR_IMAGE};g" ${ECS_TASK_DEFINITION_TEMPLATE} > ${ECS_TASK}
 aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file://${ECS_TASK}
 
 TASK_REPLICA=$(aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | jq '.taskDefinition.revision')
 DESIRED_COUNT=$(aws ecs describe-services --service ${SERVICE_NAME} --cluster ${ECS_CLUSTER} | jq '.services[0]|.desiredCount')
+
+#Update new task version
 
 aws ecs update-service --cluster ${ECS_CLUSTER} \
                        --service ${SERVICE_NAME} \
