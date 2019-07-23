ee425fcfdd453fef5af1be32d1154d0384fe409c
Ossi <compvter2@gmail.com>
2019-07-23 13:27:47 +0300

    debug pipeline

--- AIS/WebSocketReader/deploy.sh
+++ AIS/WebSocketReader/deploy.sh
@@ -7,6 +7,7 @@ ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY" --region "$AWS_DEFAULT_REGION")
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 NEW_REVISION= $(echo aws ecs register-task-definition --region "$AWS_DEFAULT_REGION" --cli-input-json "$NEW_TASK_DEFINTIION" | jq '.revision' )
+echo updating revision to: $NEW_REVISION
 aws ecs update-service  aws ecs update-service --cluster ${ECS_CLUSTER} \
                        --service ${SERVICE_NAME} \
                        --task-definition ${TASK_FAMILY}:${NEW_REVISION} \
