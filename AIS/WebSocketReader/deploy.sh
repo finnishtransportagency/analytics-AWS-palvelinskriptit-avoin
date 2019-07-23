cf4b0ec01c431ce189adafc50906e2b9328772aa
Ossi <compvter2@gmail.com>
2019-07-23 12:23:10 +0300

    quicker deployment script

--- AIS/WebSocketReader/deploy.sh
+++ AIS/WebSocketReader/deploy.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-#Script to get current task defintion, replicate it with new container version
+#Script to get current task defintion, replicate it with new container version and add to revision number
 set -e
 
-ECS_TASK_DEFINITION_TEMPLATE="task.json"
-ECS_TASK="task-ecs.json"
-ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${CODEBUILD_RESOLVED_SOURCE_VERSION}"
-
-# Create task definition replica
-
-sed -e "s;%ECR_IMAGE%;${ECR_IMAGE};g" ${ECS_TASK_DEFINITION_TEMPLATE} > ${ECS_TASK}
-aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file://${ECS_TASK}
-
-TASK_REPLICA=$(aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | jq '.taskDefinition.revision')
-DESIRED_COUNT=$(aws ecs describe-services --service ${SERVICE_NAME} --cluster ${ECS_CLUSTER} | jq '.services[0]|.desiredCount')
-
-#Update new task version
-
-aws ecs update-service --cluster ${ECS_CLUSTER} \
-                       --service ${SERVICE_NAME} \
-                       --task-definition ${TASK_FAMILY}:${TASK_REPLICA} \
-                       --desired-count ${DESIRED_COUNT}
+TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY" --region "us-east-1")
+NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
+aws ecs register-task-definition --region "us-east-1" --cli-input-json "$NEW_TASK_DEFINTIION"
