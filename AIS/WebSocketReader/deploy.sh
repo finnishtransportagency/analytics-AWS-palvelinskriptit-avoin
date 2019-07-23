014f364d9aa1009661c7c40a9ebf0abbdfba8611
Ossi <compvter2@gmail.com>
2019-07-23 14:22:14 +0300

    automation removed echo

--- AIS/WebSocketReader/deploy.sh
+++ AIS/WebSocketReader/deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#Script to get current task defintion, replicate it with new container version and add to revision number
+#Script to get current task defintion, and based on that we add new ecr image to old template and remove attributes that are not needed, then we send new task info, get new revisio number from output and update service
 set -e
 ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${CODEBUILD_RESOLVED_SOURCE_VERSION}"
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY" --region "$AWS_DEFAULT_REGION")
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 NEW_TASK_INFO=$(aws ecs register-task-definition --region "$AWS_DEFAULT_REGION" --cli-input-json "$NEW_TASK_DEFINTIION")
 NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')
-echo updating revision to: $NEW_REVISION
 aws ecs update-service  aws ecs update-service --cluster ${ECS_CLUSTER} \
                        --service ${SERVICE_NAME} \
                        --task-definition ${TASK_FAMILY}:${NEW_REVISION} 
