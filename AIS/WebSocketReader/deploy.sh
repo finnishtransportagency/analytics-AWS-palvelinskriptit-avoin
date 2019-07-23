60fd7b8534da86c59ad65539ba2756c67d3f8962
Ossi <compvter2@gmail.com>
2019-07-23 12:50:09 +0300

    var2

--- AIS/WebSocketReader/deploy.sh
+++ AIS/WebSocketReader/deploy.sh
@@ -2,6 +2,7 @@
 
 #Script to get current task defintion, replicate it with new container version and add to revision number
 set -e
+ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${CODEBUILD_RESOLVED_SOURCE_VERSION}"
 
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY" --region "$AWS_DEFAULT_REGION")
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
