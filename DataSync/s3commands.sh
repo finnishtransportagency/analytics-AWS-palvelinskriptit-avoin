64f58bba2a52fd26e8ca913cdc2382ea0e803372
Ossi <compvter2@gmail.com>
2019-07-31 15:18:34 +0300

    removed script

--- DataSync/s3commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#source where commands are fetched
-sourcebucket=$1
-echo using souce bucket to fetch commands ${sourcebucket}
-aws s3 cp s3://${sourcebucket}/commands.sh .
-sh commands.sh
-
-
