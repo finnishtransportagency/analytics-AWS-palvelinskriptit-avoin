895959cbebab9b25c096361a620951e38e358bf0
oappicgi <ossi.ala-peijari@cgi.com>
2023-01-10 19:46:07 +0200

    reverted s3 path

--- AIS/WebSocketReader/main.go
+++ AIS/WebSocketReader/main.go
@@ -144,9 +144,9 @@ func StoragePathAndFileNaming(parsed bool) string {
 	currentTime := time.Now()
 	var dateSaltprefix = currentTime.Format("2006/01/02") + "/" + strconv.FormatInt(time.Now().UnixNano(), 10) + randomHex() + "AIS.json.gz"
 	if parsed == true {
-		s3prefix = "test/parsed/" + dateSaltprefix
+		s3prefix = "parsed/" + dateSaltprefix
 	} else {
-		s3prefix = "test/raw/" + dateSaltprefix
+		s3prefix = "raw/" + dateSaltprefix
 	}
 	return s3prefix
 }
